57b7aab6294e7375d6ed13c45dbafa78a8314a92
Lucas Dias <lucas.dortiz17@gmail.com>
2024-06-01 11:09:13 -0300

    feat: migrated to nx

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./login/login.component').then((c) => c.LoginComponent)
+    },
+    {
+        path: 'participantes',
+        loadComponent: () => import('./attendee/attendee.component').then((c) => c.AttendeeComponent)
+    }
+];
